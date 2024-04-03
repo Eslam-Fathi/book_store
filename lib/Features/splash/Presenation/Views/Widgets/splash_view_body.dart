049aa89980374ac7907acb342e065b0491f5d9de
Eslam Fathi <efathi2511@gmail.com>
2024-04-03 16:47:30 +0200

    Add initial project files and configurations
    This commit adds initial project files including .gitignore, README.md,
    pubspec.yaml, analysis_options.yaml, main.dart, splash_view.dart, and
    splash_view_body.dart. It also includes necessary dependencies and dev
    dependencies for the Flutter project.

---
+++ lib/Features/splash/Presenation/Views/Widgets/splash_view_body.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SplasgViewBody extends StatelessWidget {
+  const SplasgViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [],
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:book_store/Features/splash/Presenation/Views/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const BookStoreApp());
+}
+
+class BookStoreApp extends StatelessWidget {
+  const BookStoreApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const SplashView(),
+    );
+  }
+}
