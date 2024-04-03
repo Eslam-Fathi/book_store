b58a3499c77977f28300ecb94c005b82c9e6c681
Eslam Fathi <efathi2511@gmail.com>
2024-04-03 19:28:32 +0200

    Add animated brand logo widget and update assets
    Added a new AnimatedBrandLogo widget for splash screen animation and updated
    assets with new brand logo and splash image. Also updated main theme to dark
    mode.

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,5 +1,6 @@
 class AssetsData {
-  static const String appLogo = 'assets/svg/applogo.svg';
   static const String logo = 'assets/images/logo.png';
-  // static const String appLogo = 'assets/svg/applogo.svg';
+  static const String brandLogo = 'assets/images/brandLogo.png';
+  static const String splashImage = 'assets/svg/BrandLogo.svg';
+  static const String appIcon = 'assets/svg/LogoSvg.svg';
 }
