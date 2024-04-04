b9db7845145956ea3bff969ce8c143472b4671af
Eslam Fathi <efathi2511@gmail.com>
2024-04-04 14:58:05 +0200

    Add new home and splash views with animations
    Introduce new home and changed splash views then added animated brand logo and transition
    animations. Implement sliding and fading animations for splash view.
    
    implemented a method to navigate to home with transition animation

--- lib/Features/Splash/Presentation/Views/splash_view.dart
+++ lib/Features/Splash/Presentation/Views/splash_view.dart
@@ -1,4 +1,4 @@
-import 'package:book_store/Features/splash/Presenation/Views/Widgets/splash_view_body.dart';
+import 'package:book_store/Features/Splash/Presentation/Views/Widgets/splash_view_body.dart';
 import 'package:flutter/material.dart';
 
 class SplashView extends StatelessWidget {
@@ -7,7 +7,7 @@ class SplashView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: SplasgViewBody(),
+      body: SplashViewBody(),
     );
   }
 }
